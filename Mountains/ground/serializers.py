2604314c2614b049ba22987fa1ab147fbe218b1e
BlackSieve <ivansarinov25@gmail.com>
2024-10-09 20:06:56 +0300

    Сделаны Serializer для Coord,User,Image,LevelPoint

---
+++ Mountains/ground/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from .models import StatusAdd, Coord, Image, User, LevelPoint
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'full_name',
+            'email',
+            'phone',
+        )
+
+
+class CoordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coord
+        fields = (
+            'latitude',
+            'longitube',
+            'height'
+        )
+
+
+class LevelPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LevelPoint
+        fields = (
+            'winter_level',
+            'summer_level',
+            'spring_level',
+            'autumn_level'
+        )
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = (
+            'title',
+            'img'
+        )
