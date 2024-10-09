d68e11809907179354c40ca50cc55ff45efb580c
BlackSieve <ivansarinov25@gmail.com>
2024-10-09 20:06:09 +0300

    Сделаны APIView для Coord,User,Image,LevelPoint

--- Mountains/ground/views.py
+++ Mountains/ground/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import LevelPoint, Image, User, Coord, StatusAdd
+from .serializers import ImageSerializer, CoordSerializer, UserSerializer, LevelPointSerializer
 
-# Create your views here.
+
+class UserAPIView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordAPIView(viewsets.ModelViewSet):
+    queryset = Coord.objects.all()
+    serializer_class = CoordSerializer
+
+
+class LevelAPIView(viewsets.ModelViewSet):
+    queryset = LevelPoint.objects.all()
+    serializer_class = LevelPointSerializer
+
+
+class ImageAPIView(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
